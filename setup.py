ca1f02b632cb4ae35171ef5d3f270de151901c31
jarbasai <jarbasai@mailfence.com>
2018-04-09 03:59:36 +0100

    dependencies

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(
     name='shared_camera',
-    version='0.6.1',
+    version='0.6.2',
     packages=['shared_camera'],
+    install_requires=["imutils"],
     url='https://github.com/JarbasAl/shared_camera',
     license='MIT',
     author='jarbasAi',
