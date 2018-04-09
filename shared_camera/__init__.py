60eae5121dd027eb5e46482a4eab4c4b77e33364
jarbasai <jarbasai@mailfence.com>
2018-04-09 17:12:14 +0100

    auto_start configurable

--- shared_camera/__init__.py
+++ shared_camera/__init__.py
@@ -4,17 +4,26 @@ from shared_camera.client import CameraClient
 
 class Camera(object):
     def __init__(self, stream=None, host="", port=5011, authkey="",
-                 stock=None):
+                 stock=None, autostart=True):
+        self.stream = stream
+        self.host = host
+        self.port = port
+        self.authkey = authkey
+        self.stock = stock
+        if autostart:
+            self.start()
+
+    def start(self):
         self.is_main = False
         # can we start camera?
         try:
 
             # take role of camera server
-            self._camera = CameraServer(stream, host, port, authkey,
-                                        stock=stock)
+            self._camera = CameraServer(self.stream, self.host, self.port,
+                                        self.authkey, stock=self.stock)
             self.is_main = True
         except Exception as e:
-            self._camera = CameraClient(host, port, authkey)
+            self._camera = CameraClient(self.host, self.port, self.authkey)
 
     def set_notification_callback(self, callback):
         self._camera.send_notification = callback
